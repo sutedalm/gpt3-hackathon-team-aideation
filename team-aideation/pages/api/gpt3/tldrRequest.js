77c7803b13676b11506303ec3e5c9f99596f8f2d
sutedalm <37000551+sutedalm@users.noreply.github.com>
2021-04-17 20:03:45 +0200

    move gpt3 api requests into gpt3 api folder

---
+++ team-aideation/pages/api/gpt3/tldrRequest.js
@@ -0,0 +1,24 @@
+/*
+{
+	"data": {
+		"searchQuery": "Maskenpflicht wird abgesetzt..."
+	}
+}
+*/
+
+import { submitRequestTLDR } from "../../../utils/gpt3API";
+
+export default async (req, res) => {
+  const query = req.body.data.searchQuery;
+  const gpt3SecretKey = process.env.GPT3_SECRET_KEY;
+
+  if (req.method === "POST" && query) {
+    const title = await submitRequestTLDR(query, gpt3SecretKey);
+
+    res.status(200).json({
+      title,
+    });
+  } else {
+    res.status(400).send("bad request");
+  }
+};
