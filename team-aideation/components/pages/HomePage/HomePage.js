24d23ff09811acac9a61950b2ee1885b8b709315
sutedalm <37000551+sutedalm@users.noreply.github.com>
2021-04-17 16:12:33 +0200

    homepage redirects to result page on submit

--- team-aideation/components/pages/HomePage/HomePage.js
+++ team-aideation/components/pages/HomePage/HomePage.js
@@ -1,34 +1,54 @@
 import styles from "./HomePage.module.css";
 
 import React, { useState } from "react";
+import { useRouter } from "next/router";
 import Logo from "../../atoms/Logo/Logo";
 import Banner from "../../atoms/Banner/Banner";
 
 import ValueProposition from "../ValueProposition/ValueProposition";
 import { submitRequest } from "../../../utils/gpt3API";
 
+const mockResultContent = {
+  title: "Corona Zahlen in Deutschland steigen! Maskenpflicht wird verschärft!",
+  hashtags: "#love #fashion #instagood #style #photooftheday",
+  imageUrl: "./articleImage.jpg",
+  keyword: "Maskenpflicht",
+};
+
 const HomePage = () => {
   const [text, setText] = useState("");
+
+  const router = useRouter();
+
   const handleChange = (event) => {
     setText(event.target.value);
   };
 
-  const callAPI = () => {
+  const handleSubmit = (e) => {
     console.log("Article is submitted");
+    e.preventDefault();
+
     // submitRequest(text);
+    //TODO: do api request
+    const resultContent = mockResultContent;
+
+    router.push({
+      pathname: "/result",
+      query: resultContent,
+    });
   };
 
   return (
     <div className={styles.overall}>
-      <Logo/>
-      <Banner/>
+      <Logo />
+      <Banner />
       <div className={styles.TextInput}>
         <textarea
           className={styles.TextInput__inputField}
           value={text}
           onChange={handleChange}
         />
-        <button onClick={callAPI}>Submit</button>
+        <button onClick={handleSubmit}>Submit</button>
       </div>
       <ValueProposition />
     </div>
