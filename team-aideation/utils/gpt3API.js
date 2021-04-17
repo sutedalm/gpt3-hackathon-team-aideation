2f5b18f992eb206aaa6ab4c2a1a8373c04bd6ad6
Stefan <Stefan.Steg@gmail.com>
2021-04-17 15:45:00 +0200

    changed gpt3 request params

--- team-aideation/utils/gpt3API.js
+++ team-aideation/utils/gpt3API.js
@@ -21,12 +21,42 @@ async function submitRequest(params, apiKey) {
   return(json.choices[0].text)
 }
 
+function createTLDRPromt(text) {
+  return (`Der Bayerische Verwaltungsgerichtshof hat das Verbot von touristischen Tagesausflügen für Bewohner von Corona-Hotspots über einen Umkreis von 15 Kilometern hinaus in Bayern vorläufig gekippt. Die textliche Festlegung eines solchen Umkreises sei nicht deutlich genug und verstoße aller Voraussicht nach gegen den Grundsatz der Normenklarheit, entschied das Gericht am Dienstag.
+Gegen den Beschluss gibt es keine Rechtsmittel. Der Kläger, der SPD-Landtagsabgeordnete Christian Flisek, erklärte, die Entscheidung zeige, dass auch in Krisenzeiten auf den Rechtsstaat Verlass sei. Künftige Bußgeldbescheide hätten nun keine Rechtsgrundlage mehr - bei Verstößen wurden bisher 500 Euro fällig.
+###
+Zusammenfassung:
+
+Menschen in Bayern dürfen ab sofort wieder Ausflüge machen. Ein Gericht hat es erlaubt. Besonders sichere Masken, die FFP2-Masken heißen, sind aber weiter Pflicht.
+###
+Eigentlich wollten sie diese Woche Salat, Blumenkohl und Kraut einpflanzen, doch stattdessen müssen die Gärtner in Gundelfingen umdisponieren. Denn das Aprilwetter mit Minusgraden und Schneeschauern macht den schwäbischen Gartenbauern einen Strich durch die Rechnung.
+In der schwäbischen Gartenstadt wurde die Gemüsesaison schon vor Ostern eingeläutet: Die ersten Setzlinge sind bereits in die Felder gepflanzt - jetzt müssen sie mit einem Flies vor Kälte geschützt werden
+"Sind die kleinen Pflänzchen mal angewachsen, halten sie aber schon was aus", erklärt Verena Seifried von der Gemüsegärtnerei Seifried. Frostige Temperaturen könne es schließlich noch bis Mitte Mai geben. Bereits erntereif ist das Gemüse im Gewächshaus: Roter Rettich, Salat und Radieschen können seit Ostern geerntet werden, denn hier haben die Gundelfinger etwas nachgeholfen – und geheizt.
+Auch für die Obstbauern der Region bedeuten die Minusgrade aktuell mehr Arbeit. Obstbauer Ulrich Zott aus dem Landkreis Augsburg kontrolliert gerade täglich die Blüten an den Obstbäumen. An den Aprikosenbäumen lassen sich erste Schäden abschätzen: 30 Prozent sind bereits kaputtgegangen.
+###
+Zusammenfassung:
+
+Das Wetter war sehr kalt. Deshalb haben die Bauern in Schwaben Probleme. Ihre jungen Pflanzen von Obst und Gemüse gehen kaputt.
+###
+${text}
+###
+Zusammenfassung:
+
+`)
+}
+
+function createHashTagPrompt(text) {
+  return (`Text: ${text}\n\nSchlüsselwörter:`)
+}
+
 export async function submitRequestTLDR(text, apiKey) {
     var params = {
-      "prompt": `${text}\n\ntl;dr:`,
-      "max_tokens": 60,
+      "prompt": createTLDRPromt(text),
+      "max_tokens": 500,
       "temperature": 0.3,
-      "frequency_penalty": 0.8
+      "frequency_penalty": 0.2,
+      "best_of":2,
+      "stop": "###"
     };
     return submitRequest(params, apiKey)
 } 
@@ -34,10 +64,10 @@ export async function submitRequestTLDR(text, apiKey) {
 export async function submitRequestHashTag(text, apiKey) {
     text = text.replace(/\n/g, '')
     var params = {
-      "prompt": `Text: ${text}\n\nSchlüsselwörter:`,
+      "prompt": createHashTagPrompt(text),
       "max_tokens": 60,
-      "temperature": 0.3,
-      "frequency_penalty": 0.8,
+      "temperature": 0.7,
+      "frequency_penalty": 0.7,
       "top_p": 1.0,
       "presence_penalty": 0.0,
       "stop": ["\n"]
