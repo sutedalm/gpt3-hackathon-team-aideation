33d398e90bb483251e062b8392047df14715cd96
sutedalm <37000551+sutedalm@users.noreply.github.com>
2021-04-18 12:56:55 +0200

    go back to homepage on click on logo

--- team-aideation/components/atoms/Logo/Logo.js
+++ team-aideation/components/atoms/Logo/Logo.js
@@ -1,24 +1,16 @@
 import styles from "./Logo.module.css";
-
+import Link from "next/link";
 const Logo = () => {
   return (
-    <div>
-      <img
-        className={styles.br_logo}
-        src="https://img.br.de/9e956cb2-64e7-4c2d-8918-aa9422a6967d.png?rect=108%2C43%2C1024%2C1024&_v=1561046865881&w=500"
-      />
-
-      {/* <div className={styles.Banner}>
-          <h1>BR Instagram-stories generator</h1>
-          <p>Place your BR article in the text field and get some nice stories!</p>
-      </div> */}
+    <div className={styles.Logo}>
+      <Link href="/">
+        <img
+          className={styles.br_logo}
+          src="https://img.br.de/9e956cb2-64e7-4c2d-8918-aa9422a6967d.png?rect=108%2C43%2C1024%2C1024&_v=1561046865881&w=500"
+        />
+      </Link>
     </div>
   );
 };
 
 export default Logo;
-
-
-
-
-
